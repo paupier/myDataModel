a04f532ea5b9edf622a03d78433f84cd7d5feea5
paupier <ppierce2@ra.rockwell.com>
2022-11-23 08:32:17 +0100

    morning

---
+++ ProjectFiles/NetSolution/myMotorType1Behavior.cs
@@ -0,0 +1,33 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.UI;
+using FTOptix.WebUI;
+using FTOptix.Retentivity;
+using FTOptix.NativeUI;
+using FTOptix.RAEtherNetIP;
+using FTOptix.CoreBase;
+using FTOptix.CommunicationDriver;
+using FTOptix.Core;
+using FTOptix.NetLogic;
+#endregion
+
+[CustomBehavior]
+public class myMotorType1Behavior : BaseNetBehavior
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined behavior is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined behavior is stopped
+    }
+
+#region Auto-generated code, do not edit!
+    protected new myMotorType1 Node => (myMotorType1)base.Node;
+#endregion
+}
